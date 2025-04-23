7f0818afb614e8b0bf4a8f8f8eab190f91a3ecda
Jose P. Faria <jplfaria@gmail.com>
2025-04-22 21:43:19 -0500

    chore: switch to biomed-roberta-base-sapbert-nli

--- concord/local/embeddings.py
+++ concord/local/embeddings.py
@@ -1,10 +1,14 @@
 from sentence_transformers import SentenceTransformer, util
+
+_MODEL_ID = "sentence-transformers/biomed-roberta-base-sapbert-nli"
 _model = None
+
 def get_model():
     global _model
     if _model is None:
-        _model = SentenceTransformer("pritamdeka/BioLinkBERT-base-finetuned-biosimcse")
+        _model = SentenceTransformer(_MODEL_ID)
     return _model
+
 def similarity(a: str, b: str) -> float:
     emb = get_model().encode([a, b], convert_to_tensor=True, device="cpu")
     return float(util.cos_sim(emb[0], emb[1]))
